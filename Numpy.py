742383e9261a2800fbf0f16261c97568ea495da5
Sadhika0909 <abirami.somasundaram@gmail.com>
2024-10-27 10:56:52 +0100

    Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -55,5 +55,58 @@ a4=np.linspace(10,22,4)
 print(a4)
 
 #reshaping the array
-a5=a4.reshape(2,2)
+a5=a4.reshape(2,2) 
 print(a5)
+
+#Permutation 
+print(np.random.permutation(a4))
+print(np.random.permutation(a4))
+print(np.random.permutation(a4))
+
+#Random number
+ar=np.random.randint(4,20,9)
+print(ar)
+n1=np.random.permutation(ar)
+print(np.random.permutation(ar))
+aa=np.random.randint(5,35,(4,5))
+print(aa)
+
+#sorting the array
+print(n1)
+print(np.sort(n1))
+print(n1)
+
+l=[3,2,77,11,4,98]
+l.sort()
+print(l)
+
+#string slicing
+str="Sadhika is beautiful"
+print(str[1:5])
+print(str[5:15])
+
+#Array slicing 
+print(n1[2:7])
+print(n1[:8])
+print(n1[::-1])
+print(n1)
+
+#select indices
+print(n1[[4,2,3,1,6]])
+
+#conditional slicing
+print(n1[n1%2==0])
+print(n1[n1>5])
+print(n1[n1%3==0])
+
+#2D array slicing
+print(aa[0:1,1:])
+print(aa)
+print(aa[1:3,:2])
+
+#Operators with arrays
+aa1=np.arange(1,5)
+print(aa1)
+aa2=np.arange(6,10)
+print(aa2)
+print(aa1+aa2)
