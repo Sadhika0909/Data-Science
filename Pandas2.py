6fca8fecd0c4a0b4db6fb38e6b321b8b30fff2ed
Sadhika0909 <abirami.somasundaram@gmail.com>
2024-11-17 10:38:07 +0100

    Pandas2.py

--- Pandas2.py
+++ Pandas2.py
@@ -16,4 +16,30 @@ data.to_csv("Changeddata.csv")
 #slicing a set of rows amd columns
 print("Slicing your data:\n",data.iloc[9:25,2:5]) 
 
+#creating new columns in the dataframe
+data["Test"]=data["Fare"]+2
+data["Test2"]=data["Fare"]*data["Pclass"]
+print(data.head())
+
+data["S/S"]=data["Siblings/Spouses Aboard"]+5
+print(data.head())
+
+#Renaming columns
+datar=data.rename(columns={"Pclass":"Passenger Class","Parents/Children Aboard":"Parents/Children"})
+print(datar.info())
+
+#using agg() to perform multiple aggrications on columns
+print(data.agg({"Age":["min","max","median"],"Fare":["min","max","median"]}))
+
+#grouping data and calculating mean
+print(data[["Sex","Age"]].groupby("Sex").mean())
+
+#Counting the passengers in each Pclass
+print(data["Pclass"].value_counts())
+print(data["Survived"].value_counts())
+
+#Sorting data
+data.sort_values(by="Age")
+print(data.info())
+
 
