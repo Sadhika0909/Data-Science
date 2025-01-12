55fe2caf56b205dfcd4deb61b4481f31fedf7b83
Sadhika0909 <abirami.somasundaram@gmail.com>
2025-01-12 10:16:46 +0100

    Matplotlib6.py

--- Matplotlib6.py
+++ Matplotlib6.py
@@ -3,26 +3,23 @@ import matplotlib.pyplot as plt
 
 data=pd.read_csv("titanic.csv")
 
-gender=["Male","Female"]
-number=data["Sex"]
-plt.bar(gender,number,color="cyan")
+gendercount=data["Sex"].value_counts()
+plt.bar(gendercount.index,gendercount.values,color="cyan")
 plt.xlabel("Gender")
 plt.ylabel("Total amount in the titanic")
 plt.title("Total amount of male and female in the titanic")
 plt.show()
 
-g=data["Sex"]
-n=data[["Sex","Fare"]].groupby("Sex").mean()
-
-plt.bar(g,n,color="cyan")
+n=data.groupby("Sex")["Fare"].mean()
+plt.bar(n.index,n.values,color="cyan")
 plt.xlabel("Gender")
 plt.ylabel("Average fare")
 plt.title("Average fare of male and female in the titanic")
 plt.show()
 
-slices=[data["Fare"]]
-pclass=[data["Pclass"]]
-color=["r","b","m","c","g"]
-plt.pie(slices,labels=pclass,colors=color,startangle=90,shadow=True)
+
+pclass=data["Pclass"].value_counts()
+color=["r","b","m"]
+plt.pie(pclass,labels=["1st class","2nd class","3rd class"],colors=color,startangle=90,shadow=True)
 plt.title("Pie chart")
 plt.show() 
