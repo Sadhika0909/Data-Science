82c5109c37916417aa9760962d0d7012cac27a22
Sadhika0909 <abirami.somasundaram@gmail.com>
2025-03-10 18:10:47 +0100

    Add files via upload

--- Covid_time_series1.py
+++ Covid_time_series1.py
@@ -22,6 +22,43 @@ figure2.add_trace(go.Scatter(x=india_date.index,y=india_date["Cl_deaths"],fill="
 figure2.update_layout(title="Cumulative deaths in India")
 figure2.write_html("Cl_Deaths.html",auto_open=True)
 
+#daily new cases line graph India
+figure5=go.Figure()
+figure5.add_trace(go.Scatter(x=india_date.index,y=india_date["New_cases"],fill="tonexty",line_color="blue"))
+figure5.update_layout(title="New COVID-19 cases over time in India")
+figure5.write_html("Covid_new_casesI.html",auto_open=True)
 
+#daily new deaths line graph India
+figure6=go.Figure()
+figure6.add_trace(go.Scatter(x=india_date.index,y=india_date["New_deaths"],fill="tonexty",line_color="red"))
+figure6.update_layout(title="New COVID-19 death cases over time in India")
+figure6.write_html("Covid_new_deathsI.html",auto_open=True)
 
+#filtering data for US
+us=data[data["Country"]=="United States of America"]
+us_date=us.groupby("Date").sum()
 
+#US specific graph
+#Cumulative cases in US
+figure3=go.Figure()
+figure3.add_trace(go.Scatter(x=us_date.index,y=us_date["Cl_cases"],fill="tonexty",line_color="magenta"))
+figure3.update_layout(title="Cumulative cases in the USA")
+figure3.write_html("Cl_US.html",auto_open=True)
+
+#Cumulative deaths in US
+figure4=go.Figure()
+figure4.add_trace(go.Scatter(x=us_date.index,y=us_date["Cl_deaths"],fill="tonexty",line_color="blue"))
+figure4.update_layout(title="Cumulative deaths in the USA")
+figure4.write_html("Cl_DeathsUS.html",auto_open=True)
+
+#daily new cases line graph US
+figure7=go.Figure()
+figure7.add_trace(go.Scatter(x=us_date.index,y=us_date["New_cases"],fill="tonexty",line_color="yellow"))
+figure7.update_layout(title="New COVID-19 cases over time in the USA")
+figure7.write_html("Covid_new_casesUS.html",auto_open=True)
+
+#daily new deaths line graph US
+figure8=go.Figure()
+figure8.add_trace(go.Scatter(x=us_date.index,y=us_date["New_deaths"],fill="tonexty",line_color="black"))
+figure8.update_layout(title="New COVID-19 death cases over time in the USA")
+figure8.write_html("Covid_new_deathsUS.html",auto_open=True)
