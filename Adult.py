cd5fc56e8cac094a0b386f38d50d41373935339d
Sadhika0909 <abirami.somasundaram@gmail.com>
2025-03-17 18:18:07 +0100

    Adult.py

--- Adult.py
+++ Adult.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from pandas.plotting import scatter_matrix
+import plotly.graph_objects as go
 
 data=pd.read_csv("adult.csv")
 
 #renaming the columns
 data.columns=["Age","Job_type","ID_nb","Degree","Ed_exp","Mariatal_status","Job","Relationship","Race","Gender","Capital_gain","Capital_loss","Hours","Origin","Salary"]
- 
+data.rename(columns={"Job_type":"job type","ID_nb":"id nb","Ed_exp":"ed exp","Mariatal_status":"mariatal","Capital_gain": "capital gain","Capital_loss":"capital loss"},inplace=True)
+
 #printing basic informaton about the data set
 print(data.describe()) #showing summary
 print(data.info()) #showing column name and how many values there are
@@ -32,4 +34,12 @@ print(data.head())
 data["Gender"]=data["Gender"].map({" Male":0," Female":1}).astype(int)
 print(data.head())
 data["Race"]=data["Race"].map({" White":0," Black":1," Asian-Pac-Islander":2," Amer-Indian-Eskimo":3," Other":4})
-data["Mariatal_status"]=data["Mariatal_status"].map({" Married-civ-spouse":0," Never-married":1," Divorced":2," Separated":3," Widowed":4," Married-spouse-absent":5," Married-AF-spouse":6})
+data["mariatal"]=data["mariatal"].map({" Married-civ-spouse":0," Never-married":1," Divorced":2," Separated":3," Widowed":4," Married-spouse-absent":5," Married-AF-spouse":6})
+#data["Degree"]=data["Degree"].map({""})
+
+
+a=data.groupby("Race").sum()
+figure1=go.Figure()
+figure1.add_trace(go.Bar(x=a.index,y=a["Race"],fill="tonexty",line_color="red"))
+figure1.update_layout(title="Different races")
+figure1.write_html("bargraph.html",auto_open=True)
