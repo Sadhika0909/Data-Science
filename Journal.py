94a07abf9c533bbeff2d49cde017148ffb2a31d9
Sadhika0909 <abirami.somasundaram@gmail.com>
2025-04-21 18:27:22 +0200

    Add files via upload

--- Journal.py
+++ Journal.py
@@ -1,12 +1,27 @@
 from datetime import datetime
 
 def add_entry():
-    input("Write your diary entry: ")
+    entry=input("Write your diary entry: ")
     file=open("journal.txt","a")
     date=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    file.write("[{}]: {}".format(date,entry))
+    print("Your entry is saved")
+
+def view_entry():
+    file=open("journal.txt","r")
+    print("Your journal entries:")
+    count=0
+    for i in file:
+        print("{}. {}\n".format(count,i.strip()))
+        count+=1
 
 while True:
     print("My Journal")
     print("1.Add new entry\n2.View entry\n3.Exit")
     a=int(input("Choose an option: "))
+    if a==1:
+        add_entry()
+    elif a==2:
+        view_entry()
+
 
